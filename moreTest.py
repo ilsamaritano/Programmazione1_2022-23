40b576771e9de05c903aa5c5e8a82c9a14483365
Vincenzo Sammartino <vincesammartino@gmail.com>
2022-11-16 11:15:14 +0100

    1

--- moreTest.py
+++ moreTest.py
@@ -3,14 +3,16 @@ from testMy import *
 
 c = []
 
-inserisci(c, 12.0, "Colin", "Il mondo silenzioso di Nicholas Quinn", 2012, ("G", 15))
-inserisci(c, "Wowowowow","Colin","Il mondo paxxo",2012,("B", 15))
-inserisci(c, "Brodie", "Colin", "Il mondo rumoroso", 2012, ("H", 15), "Mi piace se ti muovi")
+inserisci(c, 12.0, "Colin",
+          "Il mondo silenzioso di Nicholas Quinn", 2012, ("G", 15))
+inserisci(c, "Wowowowow", "Colin", "Il mondo paxxo", 2012, ("B", 15))
+inserisci(c, "Brodie", "Colin", "Il mondo rumoroso",
+          2012, ("H", 15), "Mi piace se ti muovi")
 
 
 d = serializza(c)
 e = crea_copia(d)
-print(d)
+print("d:", d)
 newCat = d[:]
 
 
@@ -20,7 +22,6 @@ newCat = newCat.replace(",", "")
 cat = list(newCat.split("\n"))
 print(cat)
 
-
-
-
-
+d1 = d.split("', '")
+print(len(d1))
+print('d1', d1)
