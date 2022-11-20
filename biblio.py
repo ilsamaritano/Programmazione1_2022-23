9dbb4b7c3eb0067ada23d7a274fde51566d63d96
Vincenzo Sammartino <vincesammartino@gmail.com>
2022-11-20 18:14:03 +0100

    1

--- biblio.py
+++ biblio.py
@@ -23,30 +23,14 @@ def inserisci(cat, cognome, nome, titolo, anno, collocazione, note=[]):
             tupla = (cognome, nome, titolo,  collocazione, anno)
             cat.append(tupla)
             risposta = True
-        elif type(note) == str or type(note) == int:
+        elif type(note) == str:
             # Se c'è la nota controllo che sia del formato corretto e inserisco la tupla con 6 valori
             tupla = (cognome, nome, titolo, collocazione, anno,
-                     note)  # COMMENTARE IL CODICE
+                     note)
             cat.append(tupla)
             risposta = True
 
     return risposta
-
-    """ Inserisce un nuovo record (libro) nel catalogo controllando che i tipi dei
-    parametri attuali siano corretti -- non modifica maiuscole e minuscole dei parametri
-    :param cat: il catalogo da modificare
-    :param cognome: cognome dell'autore (stringa)
-    :param nome: nome dell'autore (stringa)
-    :param titolo: titolo del libro (stringa)
-    :param anno: anno di pubblicazione (intero positivo=
-    :param collocazione: tupla (stringa,intero positivo)
-    :param note: stringa (opzionale)
-    :return: True se l'inserimento e' stato effettuato con successo, False
-    altrimenti
-    :return: None se i parametri non hanno il tipo corretto
-    """
-    pass  # instruzione che non fa niente --> da sostituire con il codice
-# qui dev'esserci un errore nella funzione di test
 
 
 def serializza(cat):
@@ -55,32 +39,19 @@ def serializza(cat):
         for elemento in cat:
             riga += str(elemento) + "\n"
     return riga
-    """ Serializza un catalogo rappresentando la sequenza dei record in una singola stringa
-    La sottostringa relativa al singolo record
-    puo' usare un formato a scelta dello studente,
-    i record devono essere separati dal carattere "a capo" --> "\n"
-
-    :param cat: il catalogo da serializzare
-    :return: una stringa che rappresenta il catalogo
-    """
-    pass  # istruzione che non fa niente --> da sostituire con il codice
 
 
 def crea_copia(cat):
-    # si fa una clonazione con slice
+    # Si fa una clonazione con slice
     newCat = cat[:]
     return newCat
-    """ Crea una copia completa del catalogo cat (un clone) e lo restituisce
-    :param cat: il catalogo da clonare
-    :return: il nuovo catalogo clonato
-    """
-    pass  # istruzione che non fa niente --> da sostituire con il codice
 
 
 def sono_uguali(cat1, cat2):
     if len(cat1) != len(cat2):  # Se la lunghezza dei due cataloghi è diversa sono necessariamente diversi
         return False
     else:
+        # Creo due nuovi cataloghi con all'interno tuple contenenti solo cognome, nome, titolo e anno.
         newCat1 = []
         newCat2 = []
         for record1 in cat1:
@@ -93,16 +64,6 @@ def sono_uguali(cat1, cat2):
             if tupla not in newCat2:
                 return False
         return True
-
-    """Funzione booleana che stabilisce se due cataloghi contengono
-    esattamente gli stessi record con gli stessi dati (eccetto collocazione e nota -- che possono
-    essere divers)
-    I record possono non essere nello stesso ordine nei due cataloghi
-    :param cat1: primo catalogo da confrontare
-    :param cat2: secondo catalogo da confrontare
-    :return: True se sono uguali, False altrimenti
-    """
-    pass  # istruzione che non fa niente --> da sostituire con il codice
 
 
 def concatena(cat1, cat2):
@@ -128,44 +89,26 @@ def concatena(cat1, cat2):
 
     ordina(nuovo)  # ordine lessicografico
     return nuovo
-    """crea un nuovo catalogo concatenando cat1 e cat2 e lo restituisce come risultato --
-    se ci sono k record uguali in tutti i campi eccetto il campo "note"
-    il risultato contiene un solo record che nel campo note contiene la
-    concatenazione delle note dei k record uguali in ordine lessicografico
-    :param cat1: primo catalogo da unire (non viene modificato)
-    :param cat2: secondo catalogo da unire (non viene modificato)
-    :return: il nuovo catalogo
-    """
-
-    pass  # instruzione che non fa niente --> da sostituire con il codice
 
 
 def cancella(cat,  titolo, anno=None):
     if ((type(titolo) != str) or (anno != None and type(anno) != int)):
         return None
-    counter = 0
-    eliminati = 0
-    for elemento in cat:
-        if (elemento[2].lower() == titolo.lower() and (anno == None or elemento[3] == anno)):
-            cat.pop(counter)
-            eliminati += 1
-        counter += 1  # IL RETURN NONE
+    else:
+        counter = 0  # Counter per l'indice dell'elemento da rimuovere
+        eliminati = 0  # Counter per gli elementi rimossi
+        for elemento in cat:
+            if (elemento[2].lower() == titolo.lower() and (anno == None or elemento[3] == anno)):
+                cat.pop(counter)
+                eliminati += 1
+            counter += 1  # IL RETURN NONE
 
     return eliminati
-    """Cancella tutti i record per i quali i campi titolo (e opzionalmente anno)
-    coincidono con i parametri (titolo,anno)
-    :param cat: il catalogo da modificare
-    :param titolo: il titolo del libro (obbligatorio) che deve coincidere a meno di case (maiuscole/minuscole)
-    :param anno: l' anno di pubblicazione (opzionale)
-    :return: il numero dei record cancellati
-    :return: None se i parametri non hanno il tipo corretto
-    """
-    pass  # istruzione che non fa niente --> da sostituire con il codice
 
 
 def cerca(cat, pctitolo):
     for elemento in cat:
-        # La funzione restituisce -1 se l'elemento non viene trovato
+        # La funzione find() restituisce -1 se l'elemento non viene trovato
         # Cerca anche se il titolo viene digitato in minuscolo
         minuscolo = elemento[2].lower()
         if elemento[2].find(pctitolo) != -1 or minuscolo.find(pctitolo) != -1:
@@ -174,29 +117,10 @@ def cerca(cat, pctitolo):
         else:
             trovato = False
     return trovato
-    """Verifica che esista almeno un titolo che contiene la stringa pctitolo come sottoscringa (attenzione agli spazi bianchi
-     e a maiuscole e minuscole)
-    :param cat: il catalogo (non viene modificato)
-    :param pctitolo: la sottostringa che deve comparire nel titolo del libro
-    :return: True se c'è almeno un record che contiene pctitolo, False altrimenti
-    :return: None se i parametri non hanno il tipo corretto
-    """
-    pass  # instruzione che non fa niente --> da sostituire con il codice
 
 
-def ordina(cat):  # VA ORDINATO PER ANNO IN CASO DI CONFLITTI
+def ordina(cat):
+    # Ordino per cognome, nome, anno e infine titolo
     cat.sort(key=lambda key: (key[0], key[1], key[3], key[2]))
 
     return None
-    """ Ordina il catalogo alfabeticamente per cognome e nome e
-    (in caso di piu' opere dello stesso autore) per anno di pubblicazione e
-    infine per Titolo all'interno dello stesso anno come in:
-
-        Dexter Colin L'ultima corsa per Woodstock 2010 ....
-        Dexter Colin Il mondo silenzioso di Nicholas Quinn  2012 ...
-        Dexter Colin Niente vacanze per l'ispettore Morse 2012 ....
-        Simenon Georges Gli intrusi 2015
-    :param cat: il catalogo da ordinare
-    :return: None (la funzione modifica il catalogo e non restituisce niente)
-    """
-    pass  # instruzione che non fa niente --> da sostituire con il codice
